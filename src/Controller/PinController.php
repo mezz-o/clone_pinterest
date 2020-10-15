816a618682249475bcb84231b55405d5eed9f5e3
mezz-o <mezzo.dev@gmail.com>
2020-10-16 00:27:05 +0200

    add show method

--- src/Controller/PinController.php
+++ src/Controller/PinController.php
@@ -2,20 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class PinController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function index(PinRepository $pinRepository)
+    public function index(PinRepository $pinRepository, EntityManagerInterface $em): Response
     {
+
+        //New object hardcoded
+        // $pin1= New Pin();
+        // $pin1->setTitle('Pin 1');
+        // $pin1->setDescription('Desc 1');
+        // $em->persist($pin1);
+        // $em->flush();
 
         $pins = $pinRepository->findAll();
 
         return $this->render('pin/index.html.twig', compact('pins'));
+    }
+    /**
+     * @Route("/Pin/{id<[0-9]+>}", name="app_pin_show")
+     */
+    public function show(Pin $pin): Response
+    {
+       return $this->render('pin/pin.html.twig', compact('pin'));
     }
 }
