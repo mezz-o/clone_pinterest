5bfe6f2282def0c778e5d6a93e745bcb606bd634
mezz-o <mezzo.dev@gmail.com>
2020-11-03 19:56:44 +0100

    add registration

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,8 +12,10 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login( AuthenticationUtils $authenticationUtils): Response
     {
+
+
         if ($this->getUser()) {
             return $this->redirectToRoute('app_home');
         }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,11 +7,13 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\HasLifecycleCallbacks
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface
 {
@@ -55,6 +57,11 @@ class User implements UserInterface
      * @ORM\OneToMany(targetEntity=Pin::class, mappedBy="user", orphanRemoval=true)
      */
     private $pins;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -196,5 +203,17 @@ class User implements UserInterface
     public function getFullName()
     {
         return $this->firstName ." ". $this->lastName;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
     }
 }
